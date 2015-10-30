d9777ce5f6b235269dd466e14e9ca7fddcb67d9c
Nandini Sridharan <nandini9590@gmail.com>
2015-10-29 21:21:25 -0500

    parameters and array

---
+++ launch.sh
@@ -0,0 +1,6 @@
+#!/bin/bash 
+#creating instances with parameters
+declare -a instance_id 
+mapfile -t instance_id  < <(aws ec2 run-instances --image-id $1 --count $2 --instance-type $3 --key-name $4 --security-group-ids $5 --subnet-id $6 --associate-public-ip-address --iam-instance-profile Name=$7 --user-data file:///home/controller/Documents/Nandini_setup/MP1_Environment_Setup/install-env.sh --output table | grep InstanceId | sed "s/|//g" | tr -d ' ' | sed "s/InstanceId//g")
+echo ${instance_id[@]} 
+aws ec2 wait instance-running --instance-ids ${instance_id[@]}
