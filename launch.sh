f6ba19d1d6c6e211fc3756bcfcf636ed0395d66f
nandini <nandini9590@gmail.com>
2015-11-08 00:26:11 -0600

    adding cleanup

--- launch.sh
+++ launch.sh
@@ -1,4 +1,5 @@
 #!/bin/bash 
+./cleanup.sh
 #creating instances with parameters
 declare -a instance_id 
 mapfile -t instance_id  < <(aws ec2 run-instances --image-id $1 --count $2 --instance-type $3 --key-name $4 --security-group-ids $5 --subnet-id $6 --associate-public-ip-address --iam-instance-profile Name=$7 --user-data file:///home/controller/Documents/MP1_Environment_Setup_Final/install-webserver.sh --output table | grep InstanceId | sed "s/|//g" | tr -d ' ' | sed "s/InstanceId//g")
